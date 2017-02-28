32598c72e346de738a5b7eba397afed7537ee9fa
Bibek Kshetri <bpandayk@ucsd.edu>
2017-02-28 09:12:04 -0800

    index.html forms done

---
+++ public/js/content.js
@@ -0,0 +1,40 @@
+
+//Global variables to store form field values
+var fname;
+var lname;
+var add1;
+var add2;
+var zip;
+var city;
+var state;
+var dob;
+var phone;
+var email;
+var foodsort;
+var toysort;
+var delivery;
+var agegroup;
+var volunteer;
+var additional;
+
+function validateForm() {
+  fname = document.getElementById("fname");
+  lname = document.getElementById("lname");
+  add1 = document.getElementById("add1");
+  add2 = document.getElementById("add2");
+  zip = document.getElementById("zip");
+  city = document.getElementById("city");
+  state = document.getElementById("state");
+  dob = document.getElementById("dob");
+  phone = document.getElementById("phone");
+  email = document.getElementById("email");
+  agegroup = document.getElementByName("agegroup");
+  volunteer = document.getElementByName("beforevol");
+  additional = document.getElementById("");
+
+  if (document.getElementById().checked){
+    foodsort=true;.
+    //rate_value = document.getElementById('r1').value;
+  }
+
+}
