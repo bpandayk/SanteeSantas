84b4f125a65786d26aace4e95db11956c7b4fada
Bibek Kshetri <bpandayk@ucsd.edu>
2017-03-09 16:59:39 -0800

    seperate page

--- public/BoD/js/controlBod.js
+++ public/BoD/js/controlBod.js
@@ -2,9 +2,9 @@
 var searchName;
 var minage;
 var maxage;
-var foodsort=false;
-var toysort=false;
-var delivery=false;
+var foodsort="false";
+var toysort="false";
+var delivery="false";
 
 
 
@@ -23,22 +23,22 @@ var database = firebase.database();
 var date1 = new Date();
 var year = date1.getFullYear();
 
-var user = localStorage.getItem("firebase:authUser:AIzaSyA6JtrlqORiTv0N8UidMQ3L2kk9Jz1o_g8:[DEFAULT]");
+/*var user = localStorage.getItem("firebase:authUser:AIzaSyA6JtrlqORiTv0N8UidMQ3L2kk9Jz1o_g8:[DEFAULT]");
 var parser = JSON.parse(user);
 var UID = parser.uid;
 var username = parser.displayName;
 var email=parser.email;
 console.log(UID);
-
+*/
 var panel = '<div class="panel panel-primary">'+
-  					'<div class="panel-heading"><h3>Volunteers List for '+year+'</h3></div>'+
+  					'<div class="panel-heading"><h3 id="header">Volunteers List for '+year+'</h3></div>'+
   					'<table class="table table-hover table-bordered">'+
              '<tr>'+
               '<th>#</th>'+
               '<th>Name</th>'+
               '<th>Age</th>'+
-              '<th>Toy Sorting</th>'+
               '<th>Food Sorting</th>'+
+              '<th>Toy Sorting</th>'+
               '<th>Delivery</th>'+
               '<th>Misc</th>'+
              '</tr>';
@@ -53,15 +53,21 @@ function searchParam(){
 	maxage=document.getElementById("maxage").value;
 
 	if(document.getElementById("foodsort").checked){
-    foodsort=true;
+    foodsort="true";
+  } else {
+	  foodsort="false";
   }
 
 	if(document.getElementById("toysort").checked){
-    toysort=true;
+    toysort="true";
+  } else {
+	  toysort="false";
   }
 
 	if(document.getElementById("delivery").checked){
-    delivery=true;
+    delivery="true";
+  } else {
+	  delivery="false";
   }
 
 
@@ -71,35 +77,38 @@ function searchParam(){
 
 
 
-function ListAll(){
-
-	return firebase.database().ref('/Volunteers').once('value').then(function(snapshot) {
-  	var data = snapshot.val();
-    var name, age, toy, food, volunteer;
+function DisplayTable(data){
+	var name, age,toyval,foodval,deliveryval, toy, food, delivery;
     var key = Object.keys(data);
     var display = panel;
     var count=1;
 
 
     for(i in key) {
-			name = data[key[i]].firstname +" "+ data[key[i]].lastname;
+	  name = data[key[i]].firstname +" "+ data[key[i]].lastname;
       age=data[key[i]].Age;
-      if(data[key[i]].FoodSort == 'true'){
+	  
+	  foodval=data[key[i]].FoodSort;
+	  toyval=data[key[i]].ToySort;
+	  deliveryval=data[key[i]].Delivery;
+	 
+	  
+      if(foodval == "true"){
         food='&#10004';
       }else{
-        food='<span style="color:red">&#10008';
+        food='<span style="color:red">&#10008</span>';
       }
 
-      if(data[key[i]].ToySort == 'true'){
+      if(toyval == "true"){
         toy='&#10004';
       }else{
-        toy='<span style="color:red">&#10008';
+        toy='<span style="color:red">&#10008</span>';
       }
 
-      if(data[key[i]].Delivery == 'true'){
-        volunteer='&#10004';
+      if(deliveryval == "true"){
+        delivery='&#10004';
       }else{
-        volunteer='<span style="color:red">&#10008';
+        delivery='<span style="color:red">&#10008</span>';
       }
      
        
@@ -109,7 +118,7 @@ function ListAll(){
                     '<td>'+age +'</td>'+
                     '<td>'+food +'</td>'+
                     '<td>'+toy +'</td>'+
-                    '<td>'+volunteer +'</td>'+
+                    '<td>'+delivery +'</td>'+
                     '<td>'+'Del' +'</td>'+'</tr>';
      
  			display = display + addhtml;
@@ -119,29 +128,144 @@ function ListAll(){
 
     display=display+end;
     document.getElementById("insertDom").innerHTML=display;
+}
 
+
+function ListAll(){
+
+	return firebase.database().ref('/Volunteers').once('value').then(function(snapshot) {
+  	var data = snapshot.val();
+    DisplayTable(data);
 	});
 
 }
 
 
 function ListByParam(name,minage, maxage, foodsort, toysort, delivery){
-	if(name && !minage && !maxage && !foodsort && !toysort && !delivery){
-    var name1 = name.toUpperCase();
+	if((foodsort || toysort ||delivery)&& (!name && !minage && !maxage)){
+    //var name1 = name.toUpperCase();
+
   	var ref = firebase.database().ref("Volunteers");
- 	  ref.orderByChild("ToySort").equalTo("true").on("value", function(snapshot) {
- 		  console.log(snapshot.val())
-});
-  } else {
-    
- 
+ 	    ref.orderByChild("ToySort").equalTo(toysort).on("value", function(snapshot) {
+			var data=snapshot.val();
+			DisplayTable(data);
+        });
   }
 
 }
 
 
 function getDetail(alias){
-	
+	var ref = firebase.database().ref("Volunteers/"+alias);
+	ref.once('value').then(function(snapshot){
+		var data=snapshot.val();
+		var end='</table></div>';
+		var temp;
+		var panel = '<div class="panel panel-primary">'+
+  					'<div class="panel-heading"><h3 id="header">Details of '+data.firstname+" "+data.lastname+
+					'</h3></div>'+
+  					'<table class="table table-hover table-bordered">'+
+					'<tr>'+
+					'<td> Full Name </td>'+
+					'<td>'+data.firstname+" "+data.lastname+'</td>'+
+					'</tr>'+
+					'<tr>'+
+					'<td> Address </td>'+
+					'<td>'+data.address1+'</td>'+
+					'</tr>';
+		
+		if(data.address2!=" "){
+			temp=	'<tr>'+
+					'<td> Address 2 </td>'+
+					'<td>'+data.address2+'</td>'+
+					'</tr>';
+			panel = panel+temp;
+		}
+		
+		temp = 	'<tr>'+
+				'<td> City </td>'+
+				'<td>'+data.City+'</td>'+
+				'</tr><tr>'+
+				'<td> State </td>'+
+				'<td>'+data.State+'</td>'+
+				'</tr><tr>'+
+				'<td> Zip </td>'+
+				'<td>'+data.ZipCode+'</td>'+	
+				'</tr><tr>'+
+				'<td> Age </td>'+
+				'<td>'+data.Age+'</td>'+
+				'</tr><tr>'+
+				'<td> Date Of Birth </td>'+
+				'<td>'+data.DoB+'</td>'+
+				'</tr>';				
+					
+		panel=panel+temp;
+
+        if(data.Phone!=" "){
+			temp = '<tr>'+
+				'<td> Phone </td>'+
+				'<td>'+data.Phone+'</td>'+
+				'</tr>';	
+		panel=panel+temp;
+		}  		
+		
+		
+        if(data.Email!=" "){
+			temp = '<tr>'+
+				'<td> Email </td>'+
+				'<td>'+data.Email+'</td>'+
+				'</tr>';	
+		panel=panel+temp;
+		}  							
+
+		var val1=data.FoodSort;
+		var val2=data.ToySort;
+		var val3=data.Delivery;
+	    var food,toy,delivery;
+	  
+		if(val1 == "true"){
+			food='&#10004';
+		}else{
+			food='<span style="color:red">&#10008</span>';
+		}
+
+		if(val2 == "true"){
+			toy='&#10004';
+		}else{
+			toy='<span style="color:red">&#10008</span>';
+		}
+
+		if(val3 == "true"){
+			delivery='&#10004';
+		}else{
+			delivery='<span style="color:red">&#10008</span>';
+		}
+
+		temp = 	'<tr>'+
+				'<td> Food Sorting </td>'+
+				'<td>'+food+'</td>'+
+				'</tr><tr>'+
+				'<td> Toy Sorting </td>'+
+				'<td>'+toy+'</td>'+
+				'</tr><tr>'+
+				'<td> Delivery </td>'+
+				'<td>'+delivery+'</td>'+	
+				'</tr>';			
+		
+		
+		panel=panel+temp;
+		
+		
+        if(data.Info!=" "){
+			temp = '<tr>'+
+				'<td> Additional Questions </td>'+
+				'<td>'+data.Info+'</td>'+
+				'</tr>';	
+		panel=panel+temp;
+		}  		
+		
+    document.getElementById("insertDom").innerHTML=panel;		
+	});
 
 
 
