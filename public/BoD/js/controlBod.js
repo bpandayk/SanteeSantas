bcb543cea2e9d486bda806e06361d52a417bc253
Bibek Kshetri <bpandayk@ucsd.edu>
2017-03-10 19:00:54 -0800

    add

--- public/BoD/js/controlBod.js
+++ public/BoD/js/controlBod.js
@@ -42,7 +42,7 @@ console.log(UID);
 */
 var panel = '<div class="panel panel-primary">'+
   					'<div class="panel-heading"><h3 id="header">Volunteers List for '+year+'</h3></div>'+
-  					'<table class="table table-hover table-bordered">'+
+  					'<table class="table table-hover table-bordered" style="position:relative; overflow:scroll;">'+
              '<tr>'+
               '<th>#</th>'+
               '<th>Name</th>'+
@@ -377,14 +377,15 @@ function getDetail(alias){
             
     document.getElementById("insertDom").innerHTML=panel;	
 
-        var waiver=	'<div class="panel panel-primary">'+
+    if (ageGroup==1){
+       var waiver=	'<div class="panel panel-primary">'+
   					  '<div class="panel-heading"><h3 id="header">Waiver</h3></div>'+
              ' <div class="panel-body">'+'<div>  '+       
 							'<h2><span>Adult Volunteer Waiver Form</span></h2> '+ '<h4><span clasee="label">WAIVER, RELEASE OF ALL CLAIMS, AND HOLD HARMLESS AGREEMENT FOR ADULT PARTICIPATION IN SANTEE 									SANTAS FOUNDATION PROGRAMS.</span></h4></div>'+
 							'<div style="border:3px solid #73AD21; padding:10px;">'+ firstpart+       
  							'</div>'+
 							'<label for="photo"><h3>Photo Release</h3></label>'+
-             '<div style="border:3px solid #73AD21; padding:10px;">'+ secondpart+   
+             '<div style="border:3px solid #73AD21; padding:10px;">'+ secondpart+ adult;  
               '</div>'+
             '<div class="form-group ">'+
               '<div class="checkbox">'+
@@ -401,7 +402,40 @@ function getDetail(alias){
 
 
     document.getElementById("insertWaiver").innerHTML=waiver+buttonT;
-    	
+  
+    }
+
+
+    if(ageGroup==0){
+       var waiver=	'<div class="panel panel-primary">'+
+  					  '<div class="panel-heading"><h3 id="header">Waiver</h3></div>'+
+             ' <div class="panel-body">'+'<div>  '+       
+							'<h2><span>Adult Volunteer Waiver Form</span></h2> '+ '<h4><span clasee="label">WAIVER, RELEASE OF ALL CLAIMS, AND HOLD HARMLESS AGREEMENT FOR ADULT PARTICIPATION IN SANTEE 									SANTAS FOUNDATION PROGRAMS.</span></h4></div>'+
+							'<div style="border:3px solid #73AD21; padding:10px;">'+ firstpart+       
+ 							'</div>'+
+							'<label for="photo"><h3>Photo Release</h3></label>'+
+             '<div style="border:3px solid #73AD21; padding:10px;">'+ secondpart+ adult;  
+              '</div>'+
+            '<div class="form-group ">'+
+              '<div class="checkbox">'+
+                '<label for="agg1">'+
+                  '<input type="checkbox" id="waiver-agree" checked disabled> <span id="l0">'+
+                  'Click here to indicate that you agree to our terms and that you have read our Volunteers Waivers and Photo Release terms.</span>'+
+                '</label>'+
+              '</div>'+
+            '<div class="form-inline">'+
+              '<label for="initials" id="l1">Applicant Initial: </label>'+" "+
+							'<label for="initials" style="padding-left:20px" id="l2">Parent/Guardian Initial: </label>'+" "+
+            '</div>'+
+              '</div></div></div>';
+
+
+
+   		 document.getElementById("insertWaiver").innerHTML=waiver+buttonT;
+  
+   }
+
+ 
 	});
 }
 
@@ -446,8 +480,8 @@ function logout() {
  var firstpart='<p> Please Read Carefully!!!</p>'+
                '<p>Please read this form carefully and be aware that, in signing up and participating in the Santee Santas Foundation Programs, you will be waiving and releasing all claims or injuries arising out of these programs, that you or the other named participants might sustain. The terms “I”, “me”, and “my” also refer to parents or guardians as well as participants in the programs. In registering for these programs, you are agreeing as follows: As a participant in these programs, I recognize and acknowledge that there are certain risks of physical injury, and I agree to assume the full risk of any injuries, damages, or loss which I may sustain as a result of participating in any manner, in any and all activities connected with or associated with such programs. I further recognize and acknowledge that certain activities, such as loading and unloading packages, involving strenuous exertion are potentially hazardous activities and involve substantial risks of injury. I agree to waive and relinquish any and all claims I may have as a result of participating in these programs against the Santee Santas Foundation, their officers, agents and employees, any and all other participating or cooperating governmental units, any and all independent contractors, and any and all other persons and entities, of whatever nature, that might be directly or indirectly liable for any injuries that I might sustain while participating in these programs. (The parties described in the preceding sentence are referred to as “Released Parties” in the remainder of this Agreement.) I do hereby fully release and discharge the Santee Santas Foundation and the other Released Parties from any and all claims for injuries, damage or loss, which I may have or which may accrue to me on the account of my participation in these programs. I further agree to indemnify, hold harmless and defend the Santee Santas Foundation and any and all other Released Parties, from any and all claims resulting from injuries, damages, and all losses sustained by anyone, and arising out of, connected with, or in any way associated with my conduct and the activities of these programs. I further understand and agree that the terms such as “participation”, “programs”, and “activities”, referred to in this agreement, include all forms of physical exertion or movements of any nature while I am participating in these programs and further include the provision of or failure to provide proper instructions or supervision, the use and adjustment of any and all machinery, equipment, and apparatus and anything related to my use of the services, facilities, or premise involved in these programs, and transportation to and from any events. I understand the nature of these programs for which I am registering and have read and fully understand this Waiver, Release, and Hold Harmless Agreement. I further understand that any advisements or warnings of the particular risks of these programs that I subsequently receive will be incorporated by reference into and become a part of this agreement.</p>';
 
-var secondpart="  I understand that photographs may be taken during events in which my son or daughter may volunteer as a participant and understand that an identifiable likeness of my child may appear in such photographs. I further understand that Santee Santas may use such photographs on their website and in promotional materials. By signing this form, I hereby give the Santee Santas Foundation the absolute right and permission to use any photograph containing my child’s likeness in promotional materials and publicity efforts. I understand that the photograph(s) may be used in a publication, print ad, direct-mail piece, electronic media (e.g. video, CD-ROM,Internet, World Wide Web), or other form of promotion. I release the Santee Santas Foundation, the photographer, their offices, employees, agents, and designees from liability for any violation of any personal or proprietary right I may have in connection with such use. I further understand and agree that if I do not want Santee Santas to use any event photographs depicting my child in promotional materials or publicity efforts, it is my sole and exclusive responsibility to notify the photographer.I am 18 years of age or older. ";
+var secondpart="  I understand that photographs may be taken during events in which my son or daughter may volunteer as a participant and understand that an identifiable likeness of my child may appear in such photographs. I further understand that Santee Santas may use such photographs on their website and in promotional materials. By signing this form, I hereby give the Santee Santas Foundation the absolute right and permission to use any photograph containing my child’s likeness in promotional materials and publicity efforts. I understand that the photograph(s) may be used in a publication, print ad, direct-mail piece, electronic media (e.g. video, CD-ROM,Internet, World Wide Web), or other form of promotion. I release the Santee Santas Foundation, the photographer, their offices, employees, agents, and designees from liability for any violation of any personal or proprietary right I may have in connection with such use. I further understand and agree that if I do not want Santee Santas to use any event photographs depicting my child in promotional materials or publicity efforts, it is my sole and exclusive responsibility to notify the photographer.";
 
 
-
+var adult="I am 18 years of age or older.";
 
